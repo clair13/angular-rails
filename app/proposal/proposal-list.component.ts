34a974aaf8c194f2a36339f5c37110416ddf3c01
clair13 <theclairvoyant1313@yahoo.gr>
2016-12-07 11:40:15 +0100

    Create proposal list component

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
 	moduleId: module.id,
 	selector: 'proposal-list',
 	templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent {}
+export class ProposalListComponent {
+	proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://portfolio.george.com', 'Ruby on Rails', 150, 120, 15, 'george@iron.com')
+	proposalTwo: Proposal = new Proposal(99, 'XYZ Company', 'http://portfolio.george.com', 'Ruby on Rails', 150, 120, 15, 'george@diron.com')
+	proposalThree: Proposal = new Proposal(300, 'Something Company', 'http://portfolio.george.com', 'Ruby on Rails', 150, 120, 15, 'george@iron.com')
+
+	proposals: Proposal[] = [
+		this.proposalOne,
+		this.proposalTwo,
+		this.proposalThree
+	]
+}
